67b55d8defce9d85914ec0c7327d0ab27bd3f50a
Geoff Schmit <gcschmit@gmail.com>
2026-02-15 22:58:39 -0600

    Add shell script via LaunchAgent

---
+++ northstar/configFR.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export GENICAM_CACHE_V3_1=/tmp/tmp2;
+cd ~/Documents/GitHub/Northstar2025/northstar;
+source ./venv/bin/activate;
+while [ True ];
+   do /Users/nnrobot/Documents/GitHub/Northstar2025/northstar/reenumerate/reenumerate -v -l 0x01200000
+   nice -20 python3 __init__.py --config configFR.json --calibration calibration40686739.yml;
+   sleep 1;
+done
