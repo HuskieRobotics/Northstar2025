3e12dde6539bd7606355afe3529448fa35c069ef
Geoff Schmit <gcschmit@gmail.com>
2026-02-17 20:22:57 -0600

    Fix paths.

--- northstar/cameras/robots/competition/configBL.sh
+++ northstar/cameras/robots/competition/configBL.sh
@@ -5,6 +5,6 @@ cd ~/Documents/GitHub/Northstar2025/northstar;
 source ./venv/bin/activate;
 while [ True ];
    do /Users/nnrobot/Documents/GitHub/Northstar2025/northstar/reenumerate/reenumerate -v -l 0x03200000
-   nice -20 python3 __init__.py --config cameras/robots/practice/configBL.json --calibration calibration40708556.yml;
+   nice -20 python3 __init__.py --config cameras/robots/competition/configBL.json --calibration cameras/calibrations/calibration40708556.yml;
    sleep 1;
 done
--- northstar/cameras/robots/competition/configCenter.sh
+++ northstar/cameras/robots/competition/configCenter.sh
@@ -5,6 +5,6 @@ cd ~/Documents/GitHub/Northstar2025/northstar;
 source ./venv/bin/activate;
 while [ True ];
    do /Users/nnrobot/Documents/GitHub/Northstar2025/northstar/reenumerate/reenumerate -v -l 0x02210000
-   nice -20 python3 __init__.py --config cameras/robots/practice/configCenter.json --calibration calibration25249734.yml;
+   nice -20 python3 __init__.py --config cameras/robots/competition/configCenter.json --calibration cameras/calibrations/calibration25249734.yml;
    sleep 1;
 done
