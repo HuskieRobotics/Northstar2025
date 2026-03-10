6e27f5d483186ff2a3220f8288f21b1a1bfa93e1
Geoff Schmit <gcschmit@gmail.com>
2026-03-09 20:10:31 -0500

    Swap back-right camera.

--- northstar/cameras/robots/competition/configBR.sh
+++ northstar/cameras/robots/competition/configBR.sh
@@ -5,6 +5,6 @@ cd ~/Documents/GitHub/Northstar2025/northstar;
 source ./venv/bin/activate;
 while [ True ];
    do /Users/nnrobot/Documents/GitHub/Northstar2025/northstar/reenumerate/reenumerate -v -l 0x01200000
-   nice -20 python3 __init__.py --config cameras/robots/competition/configBR.json --calibration cameras/calibrations/calibration40686739.yml;
+   nice -20 python3 __init__.py --config cameras/robots/competition/configBR.json --calibration cameras/calibrations/calibration40708569.yml;
    sleep 1;
 done
