6763d4693eaf48c8662c32c5bc9ec6c5da30c91a
Geoff Schmit <gcschmit@gmail.com>
2026-03-10 20:59:49 -0500

    log timestamp in shell script

--- northstar/cameras/robots/competition/configBL.sh
+++ northstar/cameras/robots/competition/configBL.sh
@@ -4,7 +4,9 @@ export GENICAM_CACHE_V3_1=/tmp/tmp2;
 cd ~/Documents/GitHub/Northstar2025/northstar;
 source ./venv/bin/activate;
 while [ True ];
+   date +'%Y-%m-%d %H:%M:%S'
    do /Users/nnrobot/Documents/GitHub/Northstar2025/northstar/reenumerate/reenumerate -v -l 0x03200000
+   date +'%Y-%m-%d %H:%M:%S'
    nice -20 python3 __init__.py --config cameras/robots/competition/configBL.json --calibration cameras/calibrations/calibration40708556.yml;
    sleep 1;
 done
--- northstar/cameras/robots/competition/configBR.sh
+++ northstar/cameras/robots/competition/configBR.sh
@@ -4,7 +4,9 @@ export GENICAM_CACHE_V3_1=/tmp/tmp2;
 cd ~/Documents/GitHub/Northstar2025/northstar;
 source ./venv/bin/activate;
 while [ True ];
+   date +'%Y-%m-%d %H:%M:%S'
    do /Users/nnrobot/Documents/GitHub/Northstar2025/northstar/reenumerate/reenumerate -v -l 0x01200000
+   date +'%Y-%m-%d %H:%M:%S'
    nice -20 python3 __init__.py --config cameras/robots/competition/configBR.json --calibration cameras/calibrations/calibration40708542.yml;
    sleep 1;
 done
