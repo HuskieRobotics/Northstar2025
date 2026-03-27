599e21b6b7940f791339a29113a8eb03e34a637c
Geoff Schmit <gcschmit@gmail.com>
2026-03-26 19:32:33 -0500

    Calibrate replacement camera.

--- northstar/cameras/robots/competition/configBCR.sh
+++ northstar/cameras/robots/competition/configBCR.sh
@@ -7,7 +7,7 @@ while [ True ];
    date +'%Y-%m-%d %H:%M:%S'
    do /Users/nnrobot/Documents/GitHub/Northstar2025/northstar/reenumerate/reenumerate -v -l 0x02210000
    date +'%Y-%m-%d %H:%M:%S'
-   nice -20 python3 __init__.py --config cameras/robots/competition/configBCR.json --calibration cameras/calibrations/calibration40777399.yml;
+   nice -20 python3 __init__.py --config cameras/robots/competition/configBCR.json --calibration cameras/calibrations/calibration40708569.yml;
    date +'%Y-%m-%d %H:%M:%S'
    sleep 1;
 done
